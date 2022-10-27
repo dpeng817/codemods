1f224e27ba027d9936a364c5c21a1ae3701377bc
Chris DeCarolis <cdguitar817@gmail.com>
2022-10-27 15:33:39 -0700

    Don't run pytest on non-test files

--- run.sh
+++ run.sh
@@ -5,8 +5,10 @@ do
     python3 -m libcst.tool codemod convert_solid_to_op.ConvertSolidToOp $f && \
     python3 -m libcst.tool codemod convert_pipeline_to_job.ConvertPipelineToJob $f && \
     python3 -m libcst.tool codemod convert_composite_to_graph.ConvertCompositeToGraph $f && \
-    python3 -m libcst.tool codemod execute_pipeline_to_in_process.ConvertExecutePipeline $f && \
-    pytest $f -xvv --ff && \
+    python3 -m libcst.tool codemod execute_pipeline_to_in_process.ConvertExecutePipeline $f
+    if [[ $f == *"test"*".py" ]];then
+        pytest $f -xvv --ff
+    fi
     python3 -m autoflake --in-place --remove-all-unused-imports $f || \
     { echo "failed for $f"; exit 1; }
     pylint $f --rcfile=/Users/christopherdecarolis/dagster_2/pyproject.toml
